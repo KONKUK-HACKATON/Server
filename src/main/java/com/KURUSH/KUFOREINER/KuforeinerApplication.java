2715a072cad0ee0a32410c198f03ef8bcd87ffe1
ulsandonghun <donghoon141@naver.com>
2024-07-05 17:32:43 +0900

    [setting] : first setting

---
+++ src/main/java/com/KURUSH/KUFOREINER/KuforeinerApplication.java
@@ -0,0 +1,13 @@
+package com.KURUSH.KUFOREINER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuforeinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuforeinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KURUSH/KUFOREINER/KuforeinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KURUSH.KUFOREINER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuforeinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
